509dbbad5349f852aef4c5962846ca40ef6e0b60
Raymond Lucke <ray@vasaristudio.com>
2014-05-04 18:01:05 -0700

    Only include attr_accessible on Rails < 4

--- lib/taxonomy/tag.rb
+++ lib/taxonomy/tag.rb
@@ -1,14 +1,14 @@
 class Tag < ActiveRecord::Base
-  attr_accessible :name, :context
+  attr_accessible :name, :context if Rails::VERSION::MAJOR < 4
   attr_accessor :skip_before_destroy
-  
+
   ### ASSOCIATIONS:
   has_many :taggings, :dependent => :destroy
-  
+
   ### VALIDATIONS:
   validates :name, :presence => true, :uniqueness => {:scope => :context}
   validates :slug, :presence => true, :uniqueness => {:scope => :context}
-  
+
   before_validation :permalize
   before_validation :strip_name
   before_create  :set_default_left_and_right
@@ -20,7 +20,7 @@ class Tag < ActiveRecord::Base
     :foreign_key => Taxonomy.nested_set_options[:parent_column]
   has_many :children, :class_name => self.base_class.to_s,
     :foreign_key => Taxonomy.nested_set_options[:parent_column], :order => Taxonomy.nested_set_options[:left_column]
-                
+
   # no assignment to structure fields
   [Taxonomy.nested_set_options[:left_column], Taxonomy.nested_set_options[:right_column]].each do |column|
     module_eval <<-"end_eval", __FILE__, __LINE__
@@ -29,74 +29,74 @@ class Tag < ActiveRecord::Base
       end
     end_eval
   end
-  
+
   define_callbacks("before_move", "after_move")
 
   ### NESTED SCOPES
-  
+
   # calling parent_column_name in a where cause makes migrations explode in beta4, probably an AREL bug
   # use :conditions for now
 #  scope :roots, where(parent_column_name => nil).order(quote_column_name(Taxonomy.nested_set_options[:left_column]))
   scope :roots, where(Taxonomy.nested_set_options[:parent_column] => nil).order(Taxonomy.nested_set_options[:left_column])
   scope :leaves, where("#{Taxonomy.nested_set_options[:right_column]} - #{Taxonomy.nested_set_options[:left_column]} = 1").order(Taxonomy.nested_set_options[:left_column])
-  
+
   ### TAG SCOPES:
-  
-  scope :named, lambda { |context, name| 
-    where("context = ? AND name LIKE ?", context, name) 
+
+  scope :named, lambda { |context, name|
+    where("context = ? AND name LIKE ?", context, name)
   }
-  scope :named_any, lambda { |context, list| 
+  scope :named_any, lambda { |context, list|
     where(list.map { |tag| sanitize_sql(["name LIKE ?", tag.to_s]) }.join(" OR ")).where(sanitize_sql(["(context = ?)", context]))
   }
-  scope :named_like, lambda { |context, name| 
-    where("name LIKE ?", "%#{name}%") 
+  scope :named_like, lambda { |context, name|
+    where("name LIKE ?", "%#{name}%")
   }
-  scope :named_like_any, lambda { |context, list| 
+  scope :named_like_any, lambda { |context, list|
     where(list.map { |tag| sanitize_sql(["name LIKE ?", "%#{tag.to_s}%"]) }.join(" OR ")).where(sanitize_sql(["(context = ?)", context]))
   }
-  
+
   ### CLASS METHODS:
-  
+
   def self.root
     roots.first
   end
-  
+
   def self.find_context_with_slug!(context, slug)
     ret = self.where(:context => context, :slug => slug).first
     raise ActiveRecord::RecordNotFound if ret.nil?
     ret
   end
-  
+
   def self.find_context_with_name!(context, name)
     ret = self.where(:context => context, :name => name).first
     raise ActiveRecord::RecordNotFound if ret.nil?
     ret
   end
-  
+
   def self.find_or_create_with_like_by_name(context, name)
     named_like(context, name).first || create(:context => "#{context.singularize.to_s}", :name => name)
   end
-  
+
   def self.find_or_create_all_with_like_by_name(context, *list)
     list = [list].flatten
-    
+
     return [] if list.empty?
-    
+
     existing_tags = Tag.named_any(context, list).all
     new_tag_names = list.reject { |name| existing_tags.any? { |tag| tag.name.downcase == name.downcase } }
     created_tags  = new_tag_names.map { |name| Tag.create(:context => "#{context.singularize.to_s}", :name => name) }
-  
-    existing_tags + created_tags    
+
+    existing_tags + created_tags
   end
-  
+
   def self.valid?
     left_and_rights_valid? && no_duplicates_for_columns? && all_roots_valid?
   end
-  
+
   def self.treed_taggings_for(context, options = {})
     self.where("tags.context" => context.to_s.singularize)
   end
-  
+
   def self.left_and_rights_valid?
     self.base_class.joins("LEFT OUTER JOIN #{quoted_table_name} AS parent ON " +
         "#{quoted_table_name}.#{connection.quote_column_name(Taxonomy.nested_set_options[:parent_column])} = parent.#{primary_key}").where(
@@ -109,14 +109,14 @@ class Tag < ActiveRecord::Base
           "#{quoted_table_name}.#{connection.quote_column_name(Taxonomy.nested_set_options[:right_column])} >= parent.#{connection.quote_column_name(Taxonomy.nested_set_options[:right_column])}))"
     ).count == 0
   end
-  
+
   def self.no_duplicates_for_columns?
     [connection.quote_column_name(Taxonomy.nested_set_options[:left_column]), connection.quote_column_name(Taxonomy.nested_set_options[:right_column])].all? do |column|
       # No duplicates
       self.base_class.select("#{column}, COUNT(#{column})").group("#{column} HAVING COUNT(#{column}) > 1").first.nil?
     end
   end
-  
+
   def self.all_roots_valid?
     left = right = 0
     roots.all? do |root|
@@ -126,32 +126,32 @@ class Tag < ActiveRecord::Base
       end
     end
   end
-  
+
   # Rebuilds the left & rights if unset or invalid.  Also very useful for converting from acts_as_tree.
   def self.rebuild!
     # Don't rebuild a valid tree.
     return true if valid?
-    
+
     scope = lambda{|node|}
     if Taxonomy.nested_set_options[:scope]
-      scope = lambda{|node| 
+      scope = lambda{|node|
         scope_column_names.inject(""){|str, column_name|
           str << "AND #{connection.quote_column_name(column_name)} = #{connection.quote(node.send(column_name.to_sym))} "
         }
       }
     end
     indices = {}
-    
+
     set_left_and_rights = lambda do |node|
       # set left
       node[Taxonomy.nested_set_options[:left_column]] = indices[scope.call(node)] += 1
       # find
       find(:all, :conditions => ["#{connection.quote_column_name(Taxonomy.nested_set_options[:parent_column])} = ? #{scope.call(node)}", node], :order => "#{connection.quote_column_name(Taxonomy.nested_set_options[:left_column])}, #{connection.quote_column_name(Taxonomy.nested_set_options[:right_column])}, id").each{|n| set_left_and_rights.call(n) }
       # set right
-      node[Taxonomy.nested_set_options[:right_column]] = indices[scope.call(node)] += 1    
-      node.save!    
+      node[Taxonomy.nested_set_options[:right_column]] = indices[scope.call(node)] += 1
+      node.save!
     end
-                        
+
     # Find root node(s)
     root_nodes = find(:all, :conditions => "#{connection.quote_column_name(Taxonomy.nested_set_options[:parent_column])} IS NULL", :order => "#{connection.quote_column_name(Taxonomy.nested_set_options[:left_column])}, #{connection.quote_column_name(Taxonomy.nested_set_options[:right_column])}, id").each do |root_node|
       # setup index for this scope
@@ -159,23 +159,23 @@ class Tag < ActiveRecord::Base
       set_left_and_rights.call(root_node)
     end
   end
-  
+
   ### INSTANCE METHODS:
-  
+
   def ==(object)
     super || (object.is_a?(Tag) && name == object.name)
   end
-  
+
   def to_s
     name
   end
-  
+
   def count
     read_attribute(:count).to_i
   end
-  
+
   ### INSTANCE METHODS FOR MOVING ITEMS IN NESTED SET
-  
+
   # Returns true if this is a root node
   def root?
     parent_id.nil?
@@ -184,82 +184,82 @@ class Tag < ActiveRecord::Base
   def parent_id
     self[Taxonomy.nested_set_options[:parent_column]]
   end
-  
+
   # Value of the left column
   def left
     self[Taxonomy.nested_set_options[:left_column]]
   end
-  
+
   # Value of the right column
   def right
     self[Taxonomy.nested_set_options[:right_column]]
   end
-  
+
   # Returns the level of this object in the tree
   # root level is 0
   def level
     parent_id.nil? ? 0 : ancestors.count
   end
-  
+
   def leaf?
     !new_record? && right - left == 1
   end
-  
+
   # Returns true is this is a child node
   def child?
     !parent_id.nil?
   end
-  
+
   # Returns the array of all parents and self
   def self_and_ancestors
     self.reload
     nested_set_scope.where("#{self.class.quoted_table_name}.#{connection.quote_column_name(Taxonomy.nested_set_options[:left_column])} <= ? AND #{self.class.quoted_table_name}.#{connection.quote_column_name(Taxonomy.nested_set_options[:right_column])} >= ?", left, right)
   end
-  
+
   # Returns a set of itself and all of its nested children
   def self_and_descendants
     self.reload
     nested_set_scope.where("#{self.class.quoted_table_name}.#{connection.quote_column_name(Taxonomy.nested_set_options[:left_column])} >= ? AND #{self.class.quoted_table_name}.#{connection.quote_column_name(Taxonomy.nested_set_options[:right_column])} <= ?", left, right)
   end
-  
+
   # Returns the scope of all children of the parent, including self
   def self_and_siblings
     # Rails 3, but not really. scoped.where(Taxonomy.nested_set_options[:parent_column] => parent_id)
     nested_set_scope.where(["#{Taxonomy.nested_set_options[:parent_column]} == #{parent_id}"])
   end
-  
+
   # Check if other model is in the same scope
   def same_scope?(other)
     Array(Taxonomy.nested_set_options[:scope]).all? do |attr|
       self.send(attr) == other.send(attr)
     end
   end
-  
-  # Returns a set of all of its nested children which do not have children  
+
+  # Returns a set of all of its nested children which do not have children
   def leaves
     descendants.where "#{self.class.quoted_table_name}.#{connection.quote_column_name(Taxonomy.nested_set_options[:right_column])} - #{self.class.quoted_table_name}.#{connection.quote_column_name(Taxonomy.nested_set_options[:left_column])} = 1"
   end
 
   # Returns a set of all of its children and nested children
   def descendants
-    without_self(self_and_descendants) 
+    without_self(self_and_descendants)
   end
-  
+
   # Returns an array of all parents
   def ancestors
-    without_self(self_and_ancestors) 
+    without_self(self_and_ancestors)
   end
-  
+
   # Returns the array of all children of the parent, except self
   def siblings
     without_self(self_and_siblings)
   end
-  
+
   # Move the node to the child of another node (you can pass id only)
   def move_to_child_of(node)
     move_to node, :child
   end
-  
+
   # Find the first sibling to the left
   def left_sibling
     siblings.where("#{self.class.quoted_table_name}.#{connection.quote_column_name(Taxonomy.nested_set_options[:left_column])} < ?", left).order(
@@ -270,7 +270,7 @@ class Tag < ActiveRecord::Base
   def right_sibling
     siblings.where("#{self.class.quoted_table_name}.#{connection.quote_column_name(Taxonomy.nested_set_options[:left_column])} > ?", left).first
   end
-  
+
   # Move the node to the left of another node (you can pass id only)
   def move_to_left_of(node)
     move_to node, :left
@@ -287,17 +287,17 @@ class Tag < ActiveRecord::Base
   def move_right
     move_to_right_of right_sibling
   end
-  
+
   # Move the node to root nodes
   def move_to_root
     move_to nil, :root
   end
-  
+
   def is_descendant_of?(other)
     other.reload
     other.left < self.left && self.left < other.right && same_scope?(other)
   end
-  
+
   def is_or_is_descendant_of?(other)
     other.reload
     other.left <= self.left && self.left < other.right && same_scope?(other)
@@ -307,13 +307,13 @@ class Tag < ActiveRecord::Base
     self.reload
     self.left < other.left && other.left < self.right && same_scope?(other)
   end
-  
+
   def is_or_is_ancestor_of?(other)
     self.reload
     self.left <= other.left && other.left < self.right && same_scope?(other)
   end
-  
-  
+
+
   def move_possible?(target)
     self != target && # Can't target self
     same_scope?(target) && # can't be in different scopes
@@ -336,15 +336,15 @@ protected
       write_attribute(:slug, s)
     end
   end
-  
+
   def strip_name
     self.name = self.name.strip if self.name.present?
   end
-  
+
   def without_self(s)
     s.where("#{self.class.quoted_table_name}.#{self.class.primary_key} != ?", self)
   end
-  
+
   # on creation, set automatically lft and rgt to the end of the tree
   def set_default_left_and_right
     maxright = nested_set_scope.maximum(Taxonomy.nested_set_options[:right_column]) || 0
@@ -352,12 +352,12 @@ protected
     self[Taxonomy.nested_set_options[:left_column]] = maxright + 1
     self[Taxonomy.nested_set_options[:right_column]] = maxright + 2
   end
-  
+
   def store_new_parent
     @move_to_new_parent_id = send("#{Taxonomy.nested_set_options[:parent_column]}_changed?") ? parent_id : false
     true # force callback to return true
   end
-  
+
   def move_to_new_parent
     if @move_to_new_parent_id.nil?
       move_to_root
@@ -365,25 +365,25 @@ protected
       move_to_child_of(@move_to_new_parent_id)
     end
   end
-  
+
   # reload left, right, and parent
   def reload_nested_set
     reload(:select => "#{connection.quote_column_name(Taxonomy.nested_set_options[:left_column])}, " +
       "#{connection.quote_column_name(Taxonomy.nested_set_options[:right_column])}, #{connection.quote_column_name(Taxonomy.nested_set_options[:parent_column])}")
   end
-  
+
   # All nested set queries should use this nested_set_scope, which performs finds on
   # the base ActiveRecord class, using the :scope declared in the acts_as_nested_set
   # declaration.
   def nested_set_scope
     self.class.base_class.scoped.order(connection.quote_column_name(Taxonomy.nested_set_options[:left_column])) # options
   end
-  
+
   # Prunes a branch off of the tree, shifting all of the elements on the right
   # back to the left so the counts still work.
   def destroy_descendants
     return if right.nil? || left.nil? || skip_before_destroy
-    
+
     self.class.base_class.transaction do
       if Taxonomy.nested_set_options[:dependent] == :destroy
         descendants.each do |model|
@@ -396,7 +396,7 @@ protected
             left, right]
         )
       end
-      
+
       # update lefts and rights for remaining nodes
       diff = right - left + 1
       nested_set_scope.update_all(
@@ -407,12 +407,12 @@ protected
         ["#{connection.quote_column_name(Taxonomy.nested_set_options[:right_column])} = (#{connection.quote_column_name(Taxonomy.nested_set_options[:right_column])} - ?)", diff],
         ["#{connection.quote_column_name(Taxonomy.nested_set_options[:right_column])} > ?", right]
       )
-      
+
       # Don't allow multiple calls to destroy to corrupt the set
       self.skip_before_destroy = true
     end
   end
-  
+
   def move_to(target, position)
     raise ActiveRecord::ActiveRecordError, "You cannot move a new node" if self.new_record?
     return if run_callbacks(:before_move) == false
@@ -424,11 +424,11 @@ protected
         target = nested_set_scope.find(target)
       end
       self.reload_nested_set
-    
+
       unless position == :root || move_possible?(target)
         raise ActiveRecord::ActiveRecordError, "Impossible move, target node cannot be inside moved tree."
       end
-      
+
       bound = case position
         when :child;  target[Taxonomy.nested_set_options[:right_column]]
         when :left;   target[Taxonomy.nested_set_options[:left_column]]
@@ -436,7 +436,7 @@ protected
         when :root;   1
         else raise ActiveRecord::ActiveRecordError, "Position should be :child, :left, :right or :root ('#{position}' received)."
       end
-    
+
       if bound > self[Taxonomy.nested_set_options[:right_column]]
         bound = bound - 1
         other_bound = self[Taxonomy.nested_set_options[:right_column]] + 1
@@ -446,8 +446,8 @@ protected
 
       # there would be no change
       return if bound == self[Taxonomy.nested_set_options[:right_column]] || bound == self[Taxonomy.nested_set_options[:left_column]]
-    
-      # we have defined the boundaries of two non-overlapping intervals, 
+
+      # we have defined the boundaries of two non-overlapping intervals,
       # so sorting puts both the intervals and their boundaries in order
       a, b, c, d = [self[Taxonomy.nested_set_options[:left_column]], self[Taxonomy.nested_set_options[:right_column]], bound, other_bound].sort
 
@@ -456,7 +456,7 @@ protected
         when :root;   nil
         else          target[Taxonomy.nested_set_options[:parent_column]]
       end
-      
+
       self.class.base_class.update_all([
         "#{connection.quote_column_name(Taxonomy.nested_set_options[:left_column])} = CASE " +
           "WHEN #{connection.quote_column_name(Taxonomy.nested_set_options[:left_column])} BETWEEN :a AND :b " +
@@ -480,7 +480,7 @@ protected
     self.reload_nested_set
     run_callbacks(:after_move)
   end
-  
+
 private
   def self.g_returning(value)
     yield(value)
--- lib/taxonomy/tagging.rb
+++ lib/taxonomy/tagging.rb
@@ -1,12 +1,14 @@
 class Tagging < ActiveRecord::Base #:nodoc:
-  attr_accessible :tag, :tag_id,
-                  :taggable, :taggable_type, :taggable_id,
-                  :tagger, :tagger_type, :tagger_id
+  if Rails::VERSION::MAJOR < 4
+    attr_accessible :tag, :tag_id,
+                    :taggable, :taggable_type, :taggable_id,
+                    :tagger, :tagger_type, :tagger_id
+  end
 
   belongs_to :tag
   belongs_to :taggable, :polymorphic => true
   belongs_to :tagger, :polymorphic => true
-  
+
   validates :tag_id, :presence => true, :uniqueness => {:scope => [:taggable_type, :taggable_id, :tagger_id, :tagger_type]}
-  
+
 end
